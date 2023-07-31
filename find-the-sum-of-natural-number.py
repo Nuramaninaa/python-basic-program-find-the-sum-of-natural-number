f58df6965cf11ba6c58c8b7db2f94765678e1c00
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-31 14:46:54 +0800

    Add files via upload

---
+++ find-the-sum-of-natural-number.py
@@ -0,0 +1,11 @@
+#python program to find sum of natural 
+
+n = int(input("Enter the number : "))
+
+sum = 0
+
+while (n > 0) :
+     sum = sum + n
+     n = n - 1
+print("Sum of natural numbers is ", sum)     
+    
